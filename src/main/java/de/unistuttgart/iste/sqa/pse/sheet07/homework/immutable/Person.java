3fe40371d52a8baa96a0666442891efdb73b7064
yungflori <floris.schneyinck@gmail.com>
2023-12-07 15:27:13 +0100

    Update Person.java

--- src/main/java/de/unistuttgart/iste/sqa/pse/sheet07/homework/immutable/Person.java
+++ src/main/java/de/unistuttgart/iste/sqa/pse/sheet07/homework/immutable/Person.java
@@ -11,23 +11,23 @@ public final class Person {
 	// @ private instance invariant name != null && name.length() > 0;
 	// @ private instance invariant birthDate != null;
 
-	private String name;
-	private Date birthDate;
+	private final String name;
+	private final Date birthDate;
 
-	/*@
-	@ requires name != null && name.length() > 0;
-	@ requires birthDate != null;
-	@ ensures this.name == name;
-	@ ensures this.birthDate == birthDate;
-	@*/
+    /*@
+    @ requires name != null && name.length() > 0;
+    @ requires birthDate != null;
+    @ ensures this.name == name;
+    @ ensures this.birthDate == birthDate;
+    @*/
 	/**
 	 * Creates a new person with the given name and birthdate.
 	 *
-	 * @param name Name of the person.
+	 * @param name      Name of the person.
 	 * @param birthDate Birth date of the person.
 	 */
-	public Person(final String name, final Date birthDate) throws IllegalArgumentException{
-		if (name == null || name.length() == 0) {
+	public Person(final String name, final Date birthDate) throws IllegalArgumentException {
+		if (name == null || name.isEmpty()) {
 			throw new IllegalArgumentException("A person may not have a null or empty name");
 		}
 		if (birthDate == null) {
@@ -41,7 +41,6 @@ public final class Person {
 	 * @return This person's name.
 	 */
 	public /*@ pure @*/ String getName() {
-
 		return name;
 	}
 
@@ -49,39 +48,7 @@ public final class Person {
 	 * @return This person's birth date.
 	 */
 	public /*@ pure @*/ Date getBirthDate() {
-
+		// Returning a defensive copy to ensure immutability
 		return new Date(birthDate.getTime());
-	}
-
-	/*@
-	@ requires name != null;
-	@ ensures this.name == name;
-	@*/
-	/**
-	 * Sets this person's name.
-	 * @param name The new name.
-	 * @throws IllegalArgumentException If the preconditions are not satisfied.
-	 */
-	public void setName(final String name) throws IllegalArgumentException {
-		if (name == null || name.length() == 0) {
-			throw new IllegalArgumentException("A person may not have a null or empty name.");
-		}
-		this.name = name;
-	}
-
-	/*@
-	@ requires birthDate != null;
-	@ ensures this.birthDate == birthDate;
-	@*/
-	/**
-	 * Sets this person's birth date.
-	 *
-	 * @param birthDate The new birth date.
-	 */
-	public void setBirthDate(final Date birthDate) {
-		if (birthDate == null) {
-			throw new IllegalArgumentException("A person's birth date must not be null.");
-		}
-		this.birthDate = birthDate;
 	}
 }
